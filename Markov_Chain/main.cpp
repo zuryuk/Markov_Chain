1a4b025cc76157683fa926f6fcce6609805ac0f4
zuryuk <luukkanen@gmail.com>
2017-08-30 23:33:12 +0300

    Remove special characters, save to worldlist, loop worldlist, create key from current word and value for next to the map

--- Markov_Chain/main.cpp
+++ Markov_Chain/main.cpp
@@ -3,30 +3,50 @@
 #include <string>
 #include <vector>
 #include <fstream>
-
+#include <algorithm>
+#include <iterator>
 using namespace std;
+bool isValidChar(char c) {
+	switch (c) {
+	case '"':
+	case ',':
+	case '.':
+	case ':':
+	case '?':
+	case '-':
+		return true;
+	default:
+		return false;
+	}
+}
 
 int main() {
 	ifstream file;
+	file.imbue(locale());
 	file.open("Markov.txt");
 	if (!file.is_open()) {
 		cout << "Error opening file" << endl;
 		return -1;
 	}
+	map<string, vector<string>> chain;
 	string word;
+	vector<string> wordlist;
 	while (file >> word) {
-		cout << word << " ";
+		word.erase(remove_if(word.begin(), word.end(), &isValidChar), word.end());
+		wordlist.push_back(word);
 	}
 	file.close();
-	map<string, vector<string>> chain;
-	vector<string> options;
-	string option;
-	options.push_back("Testi 1");
-	chain["testi"] = options;
-	chain.at("testi").push_back("Testi 2");
-	options.push_back("Testi 3");
-	chain["testi 1"] = options;
+	int x = 0;
+	for (auto const &i : wordlist) { //Only need to read values and create key entry
+		if (chain.count(i) == 0) {
+			chain[i];
+			chain.at(i).push_back(wordlist[x+1]);
+		}
+		x++;
+	}
+
 	for (auto i: chain) {
+		cout << i.first << ":";
 		for (auto x : i.second) {
 			cout << x << endl;
 		}
